6c62068ee9bbf4659a1d09a15400fbd880283e6b
iBakuman <903926441@qq.com>
2021-07-29 19:58:40 +0800

    ClassLoader MySample.java MyTest16.java MyCat.java     Chinese: 	1.MySample使用系统类加载器加载，MyCat使用加载MySample的加载器加载,但是在当前 	  类路径中不存在MyCat.class     English: 	1.MySample is loaded using the system classloader. MyCat is 	  loaded using the loader that loads MySample, but currently 	  MyCat class does not exist in the classpath

--- src/classloader/MySample.java
+++ src/classloader/MySample.java
@@ -1,8 +1,13 @@
 package classloader;
 
 public class MySample {
+    /*
+        加载MySample时MySample.class在类路径中存在，故系统类加载器加载MySample.class
+        当加载MyCat类时使用加载MySample的系统类加载器加载MyCat.class，但是MyCat.class
+        从类路径中删除了，故找不到MyCat.class
+     */
     public MySample() {
         System.out.println("MySample is loaded by: " + this.getClass().getClassLoader());
-        new MyCat();
+        new MyCat();// 在使用MyCat()时如果MyCat类以前没有被加载则使用MySample的类来加载MyCat
     }
 }
